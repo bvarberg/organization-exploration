58a516799425c9254bc57301bb4b2a89396f6ee6
Byron Varberg <byron@crema.us>
2019-12-13 00:12:53 -0600

    Remove centered decorator

--- src/components/CompanyDrawer/CompanyDrawer.stories.tsx
+++ src/components/CompanyDrawer/CompanyDrawer.stories.tsx
@@ -1,10 +1,8 @@
 import React from "react"
-import decoratorCentered from "@storybook/addon-centered"
 import { CompanyDrawer } from "."
 
 export default {
   title: "CompanyDrawer",
-  decorators: [decoratorCentered],
 }
 
 export const example = () => <CompanyDrawer companyId="124" />
