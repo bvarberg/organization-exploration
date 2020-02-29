d0ff6ecc393aef39da1ccbe76853d5ce22b8ec6a
Byron Varberg <byron@crema.us>
2020-02-29 13:03:31 -0600

    Remove unused Logger context

--- src/contexts/Logger/index.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react"
-import { Logger, createLogger } from "../../services/Logger"
-
-const defaultLogger = createLogger("mock")
-
-export const Context = createContext<Logger>(defaultLogger)
-Context.displayName = "ContextLogger"
--- src/contexts/Logger/useLogger.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react"
-import { Context } from "."
-
-export function useLogger() {
-  const logger = useContext(Context)
-
-  return { logger }
-}
