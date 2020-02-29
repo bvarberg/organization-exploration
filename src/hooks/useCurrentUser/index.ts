1855e554cced5a4c0ad370671b2afe84ab0297e5
Byron Varberg <byron@crema.us>
2020-02-29 13:16:14 -0600

    Remove unused CurrentUser concept
    For now... It is likely to return as a service.

--- src/hooks/useCurrentUser/index.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react"
-import { CurrentUser } from "../../contexts/CurrentUser"
-
-export function useCurrentUser() {
-  // TODO: Pull from localStorage first
-  const { currentUser } = useContext(CurrentUser)
-
-  return { currentUser }
-}
